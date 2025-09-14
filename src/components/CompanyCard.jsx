43743db9880e4188d534c35425de4639ee102918
Jaba-Akter <infojeba218@gmall.com>
2025-09-14 23:50:13 +0600

    done

--- src/components/CompanyCard.jsx
+++ src/components/CompanyCard.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React from "react";
 
-const CompanyCard = () => {
-    return (
-        <div>
-            <div className="card bg-base-100 w-96 shadow-sm">
-  <figure className="px-10 pt-10">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes"
-      className="rounded-xl" />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+const CompanyCard = ({alljobs}) => {
+  return (
+    <div className=" grid lg:grid-cols-3  xl:grid-cols-4 gap-5 " >
+     {alljobs.map((job)=>(
+         <div className="card border-2 bg-base-100 w-72 shadow-sm">
+        <figure className="px-10 pt-10">
+          <img
+            src={job.logo}
+            alt="Shoes"
+            className="rounded-xl" 
+          />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title"></h2>
+          <p  className=" font-bold text-xl" >
+            {job.name}
+          </p>
+          <h1>
+            {job.industry}
+          </h1>
+          <div className="card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
         </div>
-    );
+      </div>
+     ))}
+    </div>
+  );
 };
 
 export default CompanyCard;
--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import CompanyBanner from "../components/CompanyBanner";
+import CompanyCard from "../components/CompanyCard";
 
 const Company = () => {
+    const[companyjobs,setcompanyjobs]= useState([])
+    useEffect(()=>{
+        fetch("/companyjobs.json")
+        .then((res)=>res.json())
+        .then(data=>setcompanyjobs(data))
+    },[])
   return (
-   <div>
+   <div className=" bg-gray-200">
     <CompanyBanner></CompanyBanner>
+    <CompanyCard alljobs={companyjobs} ></CompanyCard>
+    
    </div>
   );
 };
